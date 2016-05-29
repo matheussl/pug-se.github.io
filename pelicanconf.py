635d52cb413f1f045c353447edb192699f15ce99
Gabriel Araujo <gabrielaraujof@outlook.com.br>
2016-05-28 23:45:18 -0300

    Initial commit.

---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'PUG-SE'
+SITENAME = 'Python User Group Sergipe'
+SITEURL = ''
+
+TIMEZONE = 'America/Recife'
+
+DEFAULT_LANG = 'pt'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS = (('Lista de Discussão', 'https://groups.google.com/forum/#!forum/pug-se'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+# Theme config
+THEME = 'theme'
+
+# Google Analytics
+GOOGLE_ANALYTICS = 'UA-43499723-1'
